3a5bde57dac3549d86d6c44ada12f95f743fb608
alves-leandro <l.alves85@live.com>
2024-03-16 00:14:50 -0300

    feat: new java exceptions

---
+++ src/exceptions/NotFoundException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class NotFoundException extends Exception {
+    public NotFoundException() {
+        super("Not found.");
+    }
+
+    public NotFoundException(String message) {
+        super("message");
+    }
+    
+}
